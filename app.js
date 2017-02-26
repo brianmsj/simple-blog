81848c621e86abb1acc63dfe5ecb0713e12fa76a
brianmsj <brianmsj@gmail.com>
2017-02-25 21:32:24 -0800

    created html and css

---
+++ app.js
@@ -0,0 +1,40 @@
+
+// 1. Create a state object
+// 2. do functions that will modify the state
+// 3. display the state to the DOM
+// 5. add our event listeners
+var state = {
+    name:
+    email:
+    subject:
+    message:
+    date:
+}
+
+
+function addBlogItem(state)
+
+
+
+deleteBlogItem()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function eventHandlers() {
+
+
+}
